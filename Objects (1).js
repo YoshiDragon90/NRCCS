f782d59e8f046e352618875249c2473f07f9bce2
YoshiDragon90 <138705100+YoshiDragon90@users.noreply.github.com>
2023-08-09 12:08:09 -0400

    Add files via upload

--- Objects (1).js
+++ Objects (1).js
@@ -21,6 +21,10 @@ let events = [
     },
 
     {
+        "event_date": "December Hiatus"
+    },
+
+    {
         "event_date": "Wednesday, January 17, 2024 5:30-7:00 P.M.", 
         "event_location": "Location: TBA",
         "event_name": "Symposium - Intergenerational Services & Programs",
@@ -99,148 +103,148 @@ let organizations = [
 
     {
         "name": "Boys & Girls Club of New Rochelle",
-        "link": href="http://www.bgcnr.org",
-        "logo": src="http://nrccs.org/wp-content/uploads/2017/01/BGC.png"
+        "link": "http://www.bgcnr.org",
+        "logo": "http://nrccs.org/wp-content/uploads/2017/01/BGC.png"
     },
 
     {
         "name": "The YMCA",
-        "link": href="http://www.nrymca.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2017/01/Y-Red-Logo.jpg"
+        "link": "http://www.nrymca.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2017/01/Y-Red-Logo.jpg"
     },
 
     {
         "name": "New Rochelle Campership Fund",
-        "link": href="http://www.campership.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2017/01/nrcf.png"
+        "link": "http://www.campership.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2017/01/nrcf.png"
     },
 
     {
         "name": "Songcatchers Inc.",
-        "link": href="http://www.songcatchers.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/songcatchers-logo.jpg"
+        "link": "http://www.songcatchers.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/songcatchers-logo.jpg"
     },
 
     {
         "name": "White Plains Hospital",
-        "link": href="http://www.wphospital.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2017/01/White_Plains_Hospital_Center_1456264.jpg"
+        "link": "http://www.wphospital.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2017/01/White_Plains_Hospital_Center_1456264.jpg"
     },
 
     {
         "name": "MBK NewRo", /*MBK - My Brothers Keeper of New Rochelle*/
-        "link": href="http://www.mbknewro.com/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/download-2.png"
+        "link": "http://www.mbknewro.com/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/download-2.png"
     },
 
     {
         "name": "New Rochelle Fund for Educational Excellence",
-        "link": href="http://www.nredfund.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/images.jpg"
+        "link": "http://www.nredfund.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/images.jpg"
     },
 
     {
         "name": "City School District of New Rochelle ",
-        "link": href="http://www.nred.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/NRLOGO.png"
+        "link": "http://www.nred.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/NRLOGO.png"
     },
 
     {
         "name": "Nepperhan Community Center, Inc.",
-        "link": href="https://www.nepperhan.org",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/neppehan.jpg"
+        "link": "https://www.nepperhan.org",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/neppehan.jpg"
     },
 
     {
         "name": "Meals-on-Wheels of New Rochelle",
-        "link": href="http://mownewrochelle.org",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/Logo-png-pdf-sample-1.jpg"
+        "link": "http://mownewrochelle.org",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/Logo-png-pdf-sample-1.jpg"
     },
 
     {
         "name": "New Rochelle Cares AIP",
-        "link": href="http://newrochellecares.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/nrcares-aip-logo.png"
+        "link": "http://newrochellecares.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/nrcares-aip-logo.png"
     },
     
     {
         "name": "Garito Manor at Union Square",
-        "link": href="http://www.garitomanor.com/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/logo.gif"
+        "link": "http://www.garitomanor.com/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/logo.gif"
 
     },
 
     {
         "name": "PTA Everychild.onevoice",
-        "link": href="http://www.nrptac.org", /* Dead Link */
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/download-3.png"
+        "link": "http://www.nrptac.org", /* Dead Link */
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/download-3.png"
     },
 
     {
         "name": "Alzheimer's Association",
-        "link": href="http://alz.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/logo_alz.jpg"
+        "link": "http://alz.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/logo_alz.jpg"
     },
 
     {
         "name": "Hope Community Services",
-        "link": href="http://www.hopecommunityservices.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/download-5.png"
+        "link": "http://www.hopecommunityservices.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/download-5.png"
     },
 
     {
         "name": "United Hebrew of New Rochelle",
-        "link": href="http://unitedhebrewgeriatric.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/download.png"
+        "link": "http://unitedhebrewgeriatric.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/download.png"
     },
 
     {
         "name": "Iona University",
-        "link": href="http://www.iona.edu/Home.aspx",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/download-1.png"
+        "link": "http://www.iona.edu/Home.aspx",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/download-1.png"
     },
 
     {
         "name": "New Rochelle Public Library",
-        "link": href="http://nrpl.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2017/01/176644a.png"
+        "link": "http://nrpl.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2017/01/176644a.png"
 
     },
 
     {
         "name": "The Guidance Center of Westchester",
-        "link": href="http://www.theguidancecenter.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/download-1.jpg"
+        "link": "http://www.theguidancecenter.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/download-1.jpg"
     },
 
     {
         "name": "New Rochelle Day Nursery",
-        "link": href="http://www.newrochelledaynursery.org/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/download.jpg"
+        "link": "http://www.newrochelledaynursery.org/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/download.jpg"
     },
 
     {
         "name": "New Rochelle Ideally Yours",
-        "link": href="http://www.newrochelleny.com/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/Document-1.png"
+        "link": "http://www.newrochelleny.com/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/Document-1.png"
     },
 
     {
         "name": "Montefiore New Rochelle Hospital",
-        "link": href="https://www.montefiorehealthsystem.org/NewRochelle",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/montefiore-logo.png"
+        "link": "https://www.montefiorehealthsystem.org/NewRochelle",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/montefiore-logo.png"
     },
 
     {
         "name": "Zonta Club of New Rochelle",
-        "link": href="http://zontanewrochelle.blogspot.com/",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/page1image1608.jpeg"
+        "link": "http://zontanewrochelle.blogspot.com/",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/page1image1608.jpeg"
     },
 
     {
         "name": "Volunteer New York!",
-        "link": href="http://volunteernewyork.org",
-        "logo": src="http://nrccs.org/wp-content/uploads/2015/06/servlet.ImageServer.jpeg"
+        "link": "http://volunteernewyork.org",
+        "logo": "http://nrccs.org/wp-content/uploads/2015/06/servlet.ImageServer.jpeg"
     }
 
 
