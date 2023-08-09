2b65de0c834c76fa516488c8595a706dba4db8ea
YoshiDragon90 <138705100+YoshiDragon90@users.noreply.github.com>
2023-08-08 22:53:41 -0400

    Add files via upload

---
+++ Objects.js
@@ -0,0 +1,64 @@
+let name = [
+    {
+        "event_date": "Wednesday, September 20, 2023 12:00-1:30 P.M.", 
+        "event_location": "New Rochelle Public Library",
+        "event_name": "Opening Meeting",
+        "event_details": "Kick-off the 2023-2024 programming year with our members. Guest speakers: TBA"
+    },
+
+    {
+        "event_date": "Wednesday, October 18, 2023 5:30-7.00 P.M.", 
+        "event_location": "Location: TBA",
+        "event_name": "Symposium - Affordable Housing",
+        "event_details": "Panelists: TBA"
+    },
+
+    {
+        "event_date": "Wednesday, November 15, 2023 12:00-1:30 P.M.", 
+        "event_location": "Zoom Link to be sent",
+        "event_name": "Monthly Meeting - Member Roundtable",
+        "event_details": "Members share ideas, programs, and activities"
+    },
+
+    {
+        "event_date": "Wednesday, January 17, 2024 5:30-7:00 P.M.", 
+        "event_location": "Location: TBA",
+        "event_name": "Symposium - Intergenerational Services & Programs",
+        "event_details": "Panelists: TBA"
+    },
+
+    {
+        "event_date": "Wednesday, February 28, 2024 12:00-1:30 P.M.", 
+        "event_location": "Location: TBA",
+        "event_name": "Monthly Meeting - Member Showcase",
+        "event_details": "One of our members agrees to host our meeting to showcase their facilities, programs, services and impact"
+    },
+
+    {
+        "event_date": "Wednesday, March 20, 2024 12:00-1:30 P.M.", 
+        "event_location": "Zoom Link to be sent",
+        "event_name": "Monthly Meeting - Member Roundtable",
+        "event_details": "Members share ideas, programs, and activities"
+    },
+
+    {
+        "event_date": "Wednesday, April 17, 2023 9:00-10:30 A.M.", 
+        "event_location": "Davenport Mansion",
+        "event_name": "NRCCS Breakfast Honoring Community Champions and Scholarship Award Recipients",
+        "event_details": "" /* no details */
+    },
+
+    {
+        "event_date": "Wednesday, May 15, 2024", 
+        "event_location": "Time, location, participants TBA",
+        "event_name": "Health and Wellness Fair",
+        "event_details": "A festive event featuring NR community health and wellness services and programs"
+    },
+
+    {
+        "event_date": "Wednesday, June 19, 2023 12:00-1:30 P.M.", 
+        "event_location": "Location: TBA",
+        "event_name": "Closing Networking Reception",
+        "event_details": "" /* no details */
+    } 
+]
